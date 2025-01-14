672b251c76dd8bd13071b70ae791e0739275aa1d
Anand <anandk683@gmail.com>
2025-01-14 18:06:53 +0530

    refactoring

--- express-middleware.js
+++ express-middleware.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
 app.use((request, response, next) => {
-console.log(request.headers)
-next()
+	console.log(request.headers)
+	next()
 })
 app.use((request, response, next) => {
-request.chance = Math.random()
-next()
+	request.chance = Math.random()
+	next()
 })
 app.get('/', (request, response) => {
-response.json({
-chance: request.chance
-})
+	response.json({
+		chance: request.chance
+	})
 })
 app.listen(3000)
